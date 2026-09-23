671c8310fe2d29666d61a5e95161ab50e3040e74
Sneha Sajjan <snehasajjan421@gmail.com>
2026-09-23 17:07:53 +0530

    Update WEIGHTS_URL to use SHA256 hash

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.write("Upload a leaf image to detect potential plant diseases in real-time.")
 device = torch.device("cpu")
 
 WEIGHTS_FILE = "plant_disease_model.pth"
-WEIGHTS_URL = "https://github.com/snehasajjan421-ops/Plant-Disease-Classification/releases/download/v1.0/plant_disease_model.pth"
+WEIGHTS_URL = "https://github.com/snehasajjan421-ops/Plant-Disease-Classification/releases/sha256:b67d46c11e0cd969339539e618af25821825ee2f5c5f929bf667deb3dc1fe698
 
 
 # Robust downloader using requests to follow GitHub Release redirects
